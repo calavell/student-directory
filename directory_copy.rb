6c552cca38f710b38ede3f14a78e9a479c96f0e9
calavell <calavell@tcd.ie>
2020-11-13 11:00:01 +0000

    duplicate refactor input_students methods to gather info on sport and country of birth

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,11 +1,20 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the name of a student"
   puts "To finish just hit return twice"
   students = []
+  counter = 0
   name = gets.chomp
   while !name.empty? do
     students << {name: name, cohort: :november}
+    puts "What is their favourite sport?"
+    sport = gets.chomp
+    students[counter][:sport] = sport
+    puts "Where were they born?"
+    country = gets.chomp
+    students[counter][:country] = country
+    counter += 1
     puts "Now we have #{students.count} students"
+    puts "Please enter the name of a student"
     name = gets.chomp
   end
   students
@@ -19,7 +28,7 @@ end
 def print_names(students)
   n2p = 0
   while n2p < students.length do
-    puts "#{students[n2p][:name]}: #{students[n2p][:cohort]}"
+    puts "#{students[n2p][:name]}, #{students[n2p][:cohort]} cohort, likes #{students[n2p][:sport]}, born in #{students[n2p][:country]}"
     n2p += 1
   end
 end
