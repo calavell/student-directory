59afa840843888f6d83463c43245547436d1745f
calavell <calavell@tcd.ie>
2020-11-13 17:24:50 +0000

    added interactive menu method

--- directory.rb
+++ directory.rb
@@ -1,3 +1,31 @@
+def interactive_menu
+  students = []
+  loop do
+  #1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. exit"
+  #2. read the input and save it into a variable
+  selection = gets.chomp
+  #3. do what the user has asked
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    print_header
+    print_names(students)
+    print_footer(students)
+  when "9"
+    exit #exit the program
+  else
+    puts "I don't know what you meant, try again"
+  end
+  end
+end
+
+
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish just hit return twice"
@@ -25,8 +53,10 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
-
+=begin
 students = input_students
 print_header
 print_names(students)
 print_footer(students)
+=end
+interactive_menu
